e689e174306b8c1b333c024c2ee679ffc7d7ec8a
Evgeniy Dolgopolov <EvgeniiDolgopolov@outlook.com>
2022-11-07 22:43:13 +0500

    feat: copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fsPromises = require('fs/promises');
+const path = require('path');
 
+const copyDir = async (originFolderPath, copyFolderPath) => {
+  if (!originFolderPath || !copyFolderPath) return;
+
+  const folderContent = await fsPromises.readdir(originFolderPath, {
+    encoding: 'utf-8',
+    withFileTypes: true,
+  });
+
+  await fsPromises.rm(copyFolderPath, { force: true, recursive: true });
+  await fsPromises.mkdir(copyFolderPath, { recursive: true });
+
+  folderContent.forEach((content) => {
+    const originContentPath = path.join(originFolderPath, content.name);
+    const copyContentPath = path.join(copyFolderPath, content.name);
+    content.isDirectory()
+      ? copyDir(originContentPath, copyContentPath)
+      : fsPromises.copyFile(originContentPath, copyContentPath);
+  });
+};
+
+const folderPath = path.resolve(__dirname, 'files');
+const folderPathCopy = path.resolve(__dirname, 'files-copy');
+
+copyDir(folderPath, folderPathCopy);
